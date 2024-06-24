87a0ed2563f5bff7553f123da661f9742604b958
ankegowda <60497313+ankegowdaja@users.noreply.github.com>
2024-06-24 14:19:30 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
 
-// Function to check if a value is within a specified range
-int range_checking(float value, float lower_value, float upper_value) 
+
+int TemperaureIsOk(float temperature)
 {
-  return (value >= lower_value && value <= upper_value);
+     if (temperature < 0 || temperature > 45) 
+     {
+        printf("Temperature out of range\n");
+        return 0;
+     }
+     return 1;
+}
+int socIsOk(float soc)
+{
+    if (soc < 20 || soc > 80) 
+    {
+        printf("State of Charge out of range!\n");
+        return 0;
+    }
+    return 1;
 }
 
-//function to check if battery parameters are okay
-int batteryIsOk(float temperature, float soc, float chargeRate)
+int chargeRateIsOk(float chargeRate)
 {
-  int tempInRange = range_checking(temperature, 0, 45);
-  int socInRange = range_checking(soc, 20, 80);
-  int chargeRateInRange = (chargeRate <= 0.8);
-
-  if (!tempInRange)
-  {
-    printf("Temperature out of range!\n");
-  }
-  if (!socInRange) 
-  {
-    printf("State of Charge out of range!\n");
-  }
-  if (!chargeRateInRange) 
-  {
-    printf("Charge Rate out of range!\n");
-  }
-  
- if(tempInRange && socInRange && chargeRateInRange)
- {
-     printf("battery is ok \n");
- }
-  printf("\n");
-  return tempInRange && socInRange && chargeRateInRange;
+    if (chargeRate > 0.8) 
+    {
+        printf("Charge Rate out of range!\n");
+        return 0;
+    }
+    return 1;
+}
+int batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+    int tempIsOk = TemperaureIsOk(temperature);
+    int stcIsOk = socIsOk(soc);
+    int crIsOk = chargeRateIsOk(chargeRate);
+    return (tempIsOk&stcIsOk&crIsOk);
 }
 
 int main() 
 {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
-  assert(!batteryIsOk(50, 85, 1.3));
 }
